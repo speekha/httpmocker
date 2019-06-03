88ddce7efed603b44181ec14df30693458156945
David Blanc <david.blanc@i-bp.fr>
2019-06-04 01:30:54 +0200

    Javadoc

--- lib/src/main/kotlin/fr/speekha/httpmocker/policies/InMemoryPolicy.kt
+++ lib/src/main/kotlin/fr/speekha/httpmocker/policies/InMemoryPolicy.kt
@@ -7,6 +7,14 @@ import java.io.InputStream
 import java.io.PipedInputStream
 import java.io.PipedOutputStream
 
+/**
+ * A filing policy allowing to configure requests programmatically. It can be used with a
+ * @see fr.speekha.httpmocker.MockResponseInterceptor like this:
+ * <pre>{@code
+ * val policy = InMemoryPolicy()
+ * val interceptor = MockResponseInterceptor(policy, policy::matchRequest)
+ * }</pre>
+ */
 class InMemoryPolicy : FilingPolicy {
 
     private val matchers = mutableMapOf<String, List<Matcher>>()
@@ -15,10 +23,18 @@ class InMemoryPolicy : FilingPolicy {
 
     override fun getPath(request: Request): String = request.url().toString()
 
+    /**
+     * Adds a new rule for a certain URL
+     * @param url the URL being matched by this rule
+     * @param matcher the detailed request and response included in this rule
+     */
     fun addMatcher(url: String, matcher: Matcher) {
         matchers[url] = (matchers[url] ?: listOf()) + matcher
     }
 
+    /**
+     * The loading function needed by the interceptor
+     */
     fun matchRequest(url: String): InputStream? = matchers[url]?.let {
         PipedInputStream().apply {
             val pipeOut = PipedOutputStream()
