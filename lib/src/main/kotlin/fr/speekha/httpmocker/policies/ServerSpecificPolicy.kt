b7972bf0939e565c9ccc05111c8d9f07c4d3f7f8
David Blanc <david.blanc@i-bp.fr>
2019-06-05 16:23:42 +0200

    Generate doc

--- lib/src/main/kotlin/fr/speekha/httpmocker/policies/ServerSpecificPolicy.kt
+++ lib/src/main/kotlin/fr/speekha/httpmocker/policies/ServerSpecificPolicy.kt
@@ -2,6 +2,9 @@ package fr.speekha.httpmocker.policies
 
 import okhttp3.Request
 
+/**
+ * Simple filing policy that uses the complete URL, including host name, to retrieve configuration files.
+ */
 class ServerSpecificPolicy : FilingPolicy {
     override fun getPath(request: Request): String {
         val url = request.url()
--- lib/src/main/kotlin/fr/speekha/httpmocker/policies/SingleFolderPolicy.kt
+++ lib/src/main/kotlin/fr/speekha/httpmocker/policies/SingleFolderPolicy.kt
@@ -2,6 +2,10 @@ package fr.speekha.httpmocker.policies
 
 import okhttp3.Request
 
+/**
+ * Simple filing policy that stores all the configuration files in the same folder. Slashes in the URL are replaced
+ * by underscores.
+ */
 class SingleFolderPolicy(private val rootFolder: String = "") : FilingPolicy {
 
     override fun getPath(request: Request): String {
