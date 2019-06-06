5ab6ad79cebe9461248bad3fe0591f5e69e3c330
David Blanc <david.e.blanc@gmail.com>
2019-06-06 23:35:14 +0200

    Set up publication to jcenter and maven local Update README Update Licence

--- README.md
+++ README.md
@@ -9,3 +9,45 @@ server errors.
 
 Thanks to the MockResponseInterceptor, network calls will not be dispatched to the network but read from JSON
 configuration files instead. The interceptor will also allow to record scenarios so they can be reused later.
+
+
+## Current Version
+
+```gradle
+// latest
+httpmocker_version = '1.0.0-SNAPSHOT'
+```
+
+## Gradle 
+
+### Jcenter 
+
+For stable versions, check that you have the `jcenter` repository. 
+
+```gradle
+// Add Jcenter to your repositories if needed
+repositories {
+	jcenter()    
+}
+```
+
+### OJO
+
+For Snapshot versions, check that you have the `OJO` snapshot repository. 
+
+```gradle
+// Add Jcenter to your repositories if needed
+repositories {
+    maven { url 'https://oss.jfrog.org/artifactory/oss-snapshot-local' }
+}
+```
+
+### Dependencies
+
+```gradle
+implementation "fr.speekha.httpmocker:mocker:1.0.0-SNAPSHOT"
+```
+
+# Quickstart
+
+TODO
--- mocker/src/main/kotlin/fr/speekha/httpmocker/MockResponseInterceptor.kt
+++ mocker/src/main/kotlin/fr/speekha/httpmocker/MockResponseInterceptor.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 David Blanc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package fr.speekha.httpmocker
 
 import com.fasterxml.jackson.databind.ObjectMapper
@@ -8,17 +24,12 @@ import fr.speekha.httpmocker.model.Matcher
 import fr.speekha.httpmocker.model.RequestDescriptor
 import fr.speekha.httpmocker.model.ResponseDescriptor
 import fr.speekha.httpmocker.policies.FilingPolicy
-import okhttp3.Interceptor
-import okhttp3.MediaType
-import okhttp3.Protocol
-import okhttp3.Request
-import okhttp3.Response
-import okhttp3.ResponseBody
+import okhttp3.*
 import java.io.File
 import java.io.FileOutputStream
 import java.io.InputStream
 import java.nio.charset.Charset
-import java.util.Locale
+import java.util.*
 
 /**
  * A OkHTTP interceptor that can let requests through or block them and answer them with predefined responses.
