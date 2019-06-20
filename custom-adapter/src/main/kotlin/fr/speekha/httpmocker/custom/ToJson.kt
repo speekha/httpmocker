a9d7e04c50c4f866c4c9aba8541304a75b585bcf
David Blanc <david.blanc@i-bp.fr>
2019-06-20 14:56:01 +0200

    Custom serialization

---
+++ custom-adapter/src/main/kotlin/fr/speekha/httpmocker/custom/ToJson.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2019 David Blanc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package fr.speekha.httpmocker.custom
+
+import fr.speekha.httpmocker.model.Header
+import fr.speekha.httpmocker.model.Matcher
+import fr.speekha.httpmocker.model.RequestDescriptor
+import fr.speekha.httpmocker.model.ResponseDescriptor
+
+fun List<Matcher>.toJson() = joinToString(separator = ", ", prefix = "[", postfix = "]") { it.toJson() }
+
+fun Matcher.toJson(): String = "{\"request\": ${request.toJson()},\"response\": ${response.toJson()}}"
+
+private fun RequestDescriptor.toJson(): String = "{" +
+        "\"method\": \"$method\"," +
+        "\"headers\": {${headers.joinToString(separator = ",") { it.toJson() }}}," +
+        "\"params\": ${params.toJson()}," +
+        "\"body\": \"$body\"" +
+        "}"
+
+private fun <K, V> Map<K, V>.toJson(): String =
+    entries.joinToString(separator = ", ", prefix = "{", postfix = "}") { "\"${it.key}\": \"${it.value}\"" }
+
+fun Header.toJson(): String = "\"$name\": \"$value\""
+
+
+private fun ResponseDescriptor.toJson(): String = "{" +
+        "\"delay\": $delay," +
+        "\"code\": $code," +
+        "\"media-type\": \"$mediaType\"," +
+        "\"headers\": {${headers.joinToString(separator = ",") { it.toJson() }}}," +
+        "\"body\": \"$body\"," +
+        "\"body-file\": \"$bodyFile\"" +
+        "}"
