6bc9b284046c89669aa1e410bccb752d8c904be0
David Blanc <david.e.blanc@gmail.com>
2019-06-26 16:54:34 +0200

    CI tasks factorization (#20)
    Factorize builds and parallelize jobs in CI

--- prepareArtifacts.sh
+++ prepareArtifacts.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
+mkdir build
 cp -r mocker/build/* build
 cp -r jackson-adapter/build/* build
 cp -r gson-adapter/build/* build
 cp -r moshi-adapter/build/* build
 cp -r custom-adapter/build/* build
-cp -r tests/build/* build
