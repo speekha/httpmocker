19337ff33c93381cf50d1a1ca5487bb424be6578
David Blanc <david.e.blanc@gmail.com>
2019-08-06 14:26:53 +0200

    Refactoring tests for readability

--- tests/src/test/kotlin/fr/speekha/httpmocker/mappers/JsonStringReaderTest.kt
+++ tests/src/test/kotlin/fr/speekha/httpmocker/mappers/JsonStringReaderTest.kt
@@ -38,6 +38,7 @@ import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
 import java.util.stream.Stream
 
+@DisplayName("Custom JSON parser")
 class JsonStringReaderTest {
 
     @Nested
