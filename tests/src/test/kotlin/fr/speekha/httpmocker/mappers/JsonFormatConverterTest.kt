b0d93fa08410d88c40a402968a5f70dc01baad3a
David Blanc <david.e.blanc@gmail.com>
2019-08-21 18:19:50 +0200

    Formatting

--- tests/src/test/kotlin/fr/speekha/httpmocker/mappers/JsonFormatConverterTest.kt
+++ tests/src/test/kotlin/fr/speekha/httpmocker/mappers/JsonFormatConverterTest.kt
@@ -24,6 +24,7 @@ import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
 import java.util.stream.Stream
 
+@Suppress("UNUSED_PARAMETER")
 @DisplayName("JSON format conversion")
 class JsonFormatConverterTest {
 
