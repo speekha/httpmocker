f51dafc367d3d0eb237e7f8086cb448c47a5929e
David Blanc <david.e.blanc@gmail.com>
2020-01-02 17:33:34 +0100

    Remove gradle daemon for publish

--- publish.sh
+++ publish.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./gradlew mocker:bintrayUpload jackson-adapter:bintrayUpload gson-adapter:bintrayUpload moshi-adapter:bintrayUpload custom-adapter:bintrayUpload kotlinx-adapter:bintrayUpload sax-adapter:bintrayUpload -Dbintray.user=$1 -Dbintray.key=$2 --stacktrace
+./gradlew --no-daemon mocker:bintrayUpload jackson-adapter:bintrayUpload gson-adapter:bintrayUpload moshi-adapter:bintrayUpload custom-adapter:bintrayUpload kotlinx-adapter:bintrayUpload sax-adapter:bintrayUpload -Dbintray.user=$1 -Dbintray.key=$2 --stacktrace
