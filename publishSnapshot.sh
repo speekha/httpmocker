14e0def5dc13db9e73d0872a01e228cf91b1fc47
David Blanc <david.e.blanc@gmail.com>
2026-05-22 17:30:58 +0200

    Update publish

--- publishSnapshot.sh
+++ publishSnapshot.sh
@@ -15,4 +15,4 @@
 # limitations under the License.
 #
 
-./gradlew --no-daemon --no-parallel mocker-core:publish mocker-okhttp:publish mocker-ktor:publish jackson-adapter:publish gson-adapter:publish custom-adapter:publish kotlinx-adapter:publish sax-adapter:publish -Dsnapshot=true -DNEXUS_USERNAME=$1 -DNEXUS_PASSWORD=$2 -DSIGNING_KEY_ID=$3 -DSIGNING_PASSWORD=$4 -DSIGNING_SECRET_KEY_RING_FILE=$5 --stacktrace
+./gradlew --no-daemon --no-parallel mocker-core:publish mocker-okhttp:publish mocker-ktor:publish jackson-adapter:publish gson-adapter:publish custom-adapter:publish kotlinx-adapter:publish sax-adapter:publish -Dsnapshot=true -DmavenCentralUsername=$1 -DmavenCentralPassword=$2 -DSIGNING_KEY_ID=$3 -DSIGNING_PASSWORD=$4 -DSIGNING_SECRET_KEY_RING_FILE=$5 --stacktrace
