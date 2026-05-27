1e77297a11cf7c84c0afdb8c4df8b59fccf422d8
David Blanc <david.e.blanc@gmail.com>
2026-05-27 15:22:23 +0200

    Remove useless code added by Gemini

--- tests/src/test/kotlin/fr/speekha/httpmocker/client/ktor/StaticMockTests.kt
+++ tests/src/test/kotlin/fr/speekha/httpmocker/client/ktor/StaticMockTests.kt
@@ -22,19 +22,17 @@ import fr.speekha.httpmocker.client.StaticMockTests
 import fr.speekha.httpmocker.client.TestWithServer
 import fr.speekha.httpmocker.ktor.builder.mockableHttpClient
 import fr.speekha.httpmocker.serialization.Mapper
-import io.ktor.client.*
-import io.ktor.client.call.*
-import io.ktor.client.engine.cio.*
-import io.ktor.client.plugins.contentnegotiation.*
-import io.ktor.client.request.*
-import io.ktor.client.statement.*
-import io.ktor.serialization.kotlinx.json.*
-import io.mockk.clearAllMocks
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.client.statement.HttpResponse
+import io.ktor.serialization.kotlinx.json.json
 import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
-import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
@@ -46,11 +44,6 @@ import org.junit.jupiter.params.provider.MethodSource
 class StaticMockTests :
     StaticMockTests<HttpResponse, HttpClient>(),
     HttpClientTester<HttpResponse, HttpClient> by KtorTests() {
-
-    @AfterEach
-    fun cleanupMocks() {
-        clearAllMocks()
-    }
 
     @Nested
     @DisplayName("Given a disabled mock interceptor")
