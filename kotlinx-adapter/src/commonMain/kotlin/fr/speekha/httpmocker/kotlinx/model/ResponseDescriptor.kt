b600475dc0f236d69e7475fbf775262148563f5f
David Blanc <david.e.blanc@gmail.com>
2026-05-27 08:39:28 +0200

    Upgrade Kotlix serialization 1.11.0

--- kotlinx-adapter/src/commonMain/kotlin/fr/speekha/httpmocker/kotlinx/model/ResponseDescriptor.kt
+++ kotlinx-adapter/src/commonMain/kotlin/fr/speekha/httpmocker/kotlinx/model/ResponseDescriptor.kt
@@ -16,12 +16,7 @@
 
 package fr.speekha.httpmocker.kotlinx.model
 
-import fr.speekha.httpmocker.serialization.BODY
-import fr.speekha.httpmocker.serialization.BODY_FILE
-import fr.speekha.httpmocker.serialization.CODE
-import fr.speekha.httpmocker.serialization.DELAY
-import fr.speekha.httpmocker.serialization.HEADERS
-import fr.speekha.httpmocker.serialization.MEDIA_TYPE
+import fr.speekha.httpmocker.serialization.*
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import fr.speekha.httpmocker.model.ResponseDescriptor as Model
