481771a09e1468d4f116b4a545f70e2b2e62163c
David Blanc <david.e.blanc@gmail.com>
2026-05-27 15:22:23 +0200

    Remove useless code added by Gemini

--- tests/src/test/kotlin/fr/speekha/httpmocker/client/RecordTests.kt
+++ tests/src/test/kotlin/fr/speekha/httpmocker/client/RecordTests.kt
@@ -16,20 +16,33 @@
 
 package fr.speekha.httpmocker.client
 
-import fr.speekha.httpmocker.*
+import fr.speekha.httpmocker.HTTP_METHOD_GET
+import fr.speekha.httpmocker.HTTP_METHOD_POST
+import fr.speekha.httpmocker.Mode
 import fr.speekha.httpmocker.Mode.ENABLED
 import fr.speekha.httpmocker.Mode.RECORD
+import fr.speekha.httpmocker.NO_RECORDER_ERROR
+import fr.speekha.httpmocker.NO_ROOT_FOLDER_ERROR
 import fr.speekha.httpmocker.assertThrows
 import fr.speekha.httpmocker.io.HttpRequest
 import fr.speekha.httpmocker.io.StreamReader
 import fr.speekha.httpmocker.io.asReader
-import fr.speekha.httpmocker.model.*
 import fr.speekha.httpmocker.model.Matcher
+import fr.speekha.httpmocker.model.NamedParameter
+import fr.speekha.httpmocker.model.NetworkError
+import fr.speekha.httpmocker.model.RequestTemplate
+import fr.speekha.httpmocker.model.ResponseDescriptor
 import fr.speekha.httpmocker.policies.FilingPolicy
+import fr.speekha.httpmocker.readAsString
 import fr.speekha.httpmocker.serialization.DEFAULT_MEDIA_TYPE
 import fr.speekha.httpmocker.serialization.Mapper
 import fr.speekha.httpmocker.serialization.readMatches
-import io.mockk.*
+import fr.speekha.httpmocker.withFile
+import io.mockk.confirmVerified
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.slot
+import io.mockk.verify
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions
@@ -43,7 +56,7 @@ import java.io.File
 import java.io.FileNotFoundException
 import java.nio.file.Files
 import java.nio.file.Path
-import java.util.*
+import java.util.Collections
 
 @Suppress("UNUSED_PARAMETER")
 abstract class RecordTests<Response : Any, Client : Any> : HttpClientTester<Response, Client> {
@@ -581,7 +594,6 @@ abstract class RecordTests<Response : Any, Client : Any> : HttpClientTester<Resp
         @AfterEach
         fun clearFolder() {
             clearTestFolder()
-            clearAllMocks()
         }
     }
 }
